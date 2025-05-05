577fa5e8ccd70ca6ca61d0e3af275595f7a14193
git-aidai-kydykbekova <aidai.kydykbekova@alatoo.edu.kg>
2025-05-05 14:49:33 +0600

    Updated filter for RefreshToken

--- src/main/java/kg/nurtelecom/coffee_sale/security/filter/CustomAuthenticationFilter.java
+++ src/main/java/kg/nurtelecom/coffee_sale/security/filter/CustomAuthenticationFilter.java
@@ -1,16 +1,18 @@
 package kg.nurtelecom.coffee_sale.security.filter;
 
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import kg.nurtelecom.coffee_sale.security.jwt.JWT;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import kg.nurtelecom.coffee_sale.entity.RefreshToken;
+import kg.nurtelecom.coffee_sale.service.RefreshTokenService;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,40 +27,47 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
-    private final JWT jwt;
+    private final RefreshTokenService refreshTokenService;
+    private final JwtUtil jwtUtil;
 
-    public CustomAuthenticationFilter(AuthenticationConfiguration authenticationConfiguration, JWT jwt) throws Exception {
+    public CustomAuthenticationFilter(AuthenticationConfiguration authenticationConfiguration, RefreshTokenService refreshTokenService, JwtUtil jwtUtil) throws Exception{
         this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
-        this.jwt = jwt;
+        this.refreshTokenService = refreshTokenService;
+        this.jwtUtil = jwtUtil;
     }
 
-
     @Override
-    public Authentication attemptAuthentication(
-            HttpServletRequest request,
-            HttpServletResponse response) throws AuthenticationException {
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException{
         String username = request.getParameter("username");
         String password = request.getParameter("password");
+
         UsernamePasswordAuthenticationToken authenticationToken =
                 new UsernamePasswordAuthenticationToken(username, password);
         return authenticationManager.authenticate(authenticationToken);
-    }
 
+    }
 
     protected void successfulAuthentication(HttpServletRequest request,
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authentication) throws IOException, ServletException {
 
-        Map<String, String> token = new HashMap<>();
         User user = (User) authentication.getPrincipal();
-        try {
-            token.put("accessToken", jwt.createJWT(
-                    user.getUsername(),
-                    user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList())));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+
+        String access_token = jwtUtil.createAccessToken(
+                user.getUsername(),
+                user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()),
+                24 * 60 * 60 * 1000);
+
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(user.getUsername());
+        String refreshTokenString = refreshToken.getToken();
+
+        Map<String, String> token = new HashMap<>();
+        token.put("accessToken", access_token);
+        token.put("token", refreshTokenString);
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(), token);
     }
@@ -68,9 +77,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
             HttpServletResponse response,
             AuthenticationException failed) throws IOException, ServletException {
 
-
         String access_token = "Unsuccessful Authentication";
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         Map<String, String> token = new HashMap<>();
         token.put("accessToken", access_token);
         response.setContentType(APPLICATION_JSON_VALUE);
